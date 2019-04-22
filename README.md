3f94e5d9028ccf51bb3a14855aab1f75ef6a3f78
ctchen <ctchen@tencent.com>
2019-04-22 17:21:14 +0800

    提交代码

--- README.md
+++ README.md
@@ -5,17 +5,25 @@
 
 > 实现 preact 的 dom 事件委托
 
-未完成，暂时先提交
-
-## Install
+## 安装
 
 ```bash
 $ npm i -S preact-dom-delegate
 ```
 
-## How it works
+## 使用说明
 
-### Source:
+在 JSX DOM 上设置 `on` 属性，值为委托处理方法，该方法会传入一个委托对象 `delegate`，仿照 jQuery 写法，使用 `delegate.on(events, selector, handler)` 方法就可以进行事件委托。
+
+在事件处理函数中执行 `return false`，可以阻止该节点后续事件委托的执行，同时会停止事件冒泡。
+
+### 支持的事件
+
+参照 React 说明，[https://reactjs.org/docs/events.html#supported-events](https://reactjs.org/docs/events.html#supported-events)，理论上可以对所有支持冒泡的事件进行委托。
+
+事件名采用驼峰形式，如 `touchstart` 事件要用 `touchStart` 进行委托。
+
+### 调用示例
 
 ```js
 // index.js
@@ -23,32 +31,35 @@ import 'preact-dom-delegate';
 
 class App extends Component {
     render() {
-        let { time } = this.state;
         return (
             <article
                 on={delegate =>
                     delegate
-                        .on('tap', 'i', function(e) {
-                            console.log('0 .mod_play_cover', this, e);
+                        .on('click', '.btn', function(e) {
+                            console.log('click .btn', this, e);
                         })
-                        .on('tap', '.mod_play_cover', function(e) {
-                            console.log('1 .mod_play_cover', this, e);
+                        .on('touchStart', '.wrap', function(e) {
+                            console.log('touchStart .wrap', this, e);
                             return false;
                         })
-                        .on('tap', function(e) {
-                            console.log('2 this', this, e);
+                        .on('touchMove', '.wrap', function(e) {
+                            console.log('touchMove .wrap', this, e);
                         })
-                        .on('tap', '.mod_app__btn', function(e) {
-                            console.log('3 .mod_app__btn', this, e);
+                        .on('touchEnd', '.wrap', function(e) {
+                            console.log('touchEnd .wrap', this, e);
                         })
-                        .on('tap', function(e) {
-                            console.log('4 this', this, e);
+                        .on('click', 'header', function(e) {
+                            console.log('click header', this, e);
+                            return false;
+                        })
+                        .on('click', function(e) {
+                            console.log('click this', this, e);
                         })
                 }
             >
                 <header>preact-dom-delegate</header>
-                <section>
-                    <span>{time ? time.toLocaleString() : ''}</span>
+                <section class="wrap">
+                    <button class="btn">按钮</button>
                 </section>
                 <footer>
                     <a href="https://github.com/xingqiao">@xingqiao</a>
@@ -59,43 +70,4 @@ class App extends Component {
 }
 
 render(<App />, document.body);
-```
-
-### Build:
-
-执行命令 `parcel build index.html`
-
-> 只有在 `parcel build` 操作或者生产环境（`NODE_ENV = 'production'`） 的情况下会自动进行内联操作
-
-### Output:
-
-```html
-<!DOCTYPE html>
-<html lang="zh-CN">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>preact-dom-delegate</title>
-        <style>
-            footer,
-            header {
-                padding: 2em;
-                background: #16f;
-                color: #fff;
-            }
-            section {
-                padding: 2em;
-                background: #fff;
-                color: #16f;
-            }
-        </style>
-    </head>
-    <body>
-        <script src="https://cdn.jsdelivr.net/npm/preact/dist/preact.min.js"></script>
-        <script>
-            parcelRequire=function(e,t,n,r){/* 代码太长，省略…… */},{"@babel/runtime/helpers/classCallCheck":"0fcM","@babel/runtime/helpers/createClass":"P8NW","@babel/runtime/helpers/possibleConstructorReturn":"0421","@babel/runtime/helpers/getPrototypeOf":"UJE0","@babel/runtime/helpers/inherits":"d4H2",preact:"OmAK"}]},{},["Focm"]);
-        </script>
-    </body>
-</html>
 ```
--- index.js
+++ index.js
@@ -1,176 +1,125 @@
-/**
- * 点击上报
- *
- * 使用说明
-    ### 加载组件
-    - 源码 import 导入
-        ```javascript
-        // 注意使用相对路径
-        import '/y.qq.com/component/m/QMEvent/src/delegate.js';
-        ```
-    - 或者直接引入js文件
-        ```html
-        <script src="//y.qq.com/component/m/QMEvent/delegate.js?max_age=2592000"></script>
-        ```
+function isFunction(obj) {
+    return typeof obj === 'function';
+}
 
-    ### 设置委托
+function isString(obj) {
+    return typeof obj === 'string';
+}
 
-    在 JSX DOM 上设置 `on` 属性，值为委托方法，如：
-
-    ```jsx
-    <div
-        on={delegate =>
-            delegate
-                .on('tap', 'i', function(e) {
-                    console.log('0 .mod_play_cover', this, e);
-                })
-                .on('tap', '.mod_play_cover', function(e) {
-                    console.log('1 .mod_play_cover', this, e);
-                    return false;
-                })
-                .on('tap', function(e) {
-                    console.log('2 this', this, e);
-                })
-                .on('tap', '.mod_app__btn', function(e) {
-                    console.log('3 .mod_app__btn', this, e);
-                })
-                .on('tap', function(e) {
-                    console.log('4 this', this, e);
-                })
-        }
-    >
-        ……
-    </div>
-    ```
-
-    - 执行顺序为从点击元素开始，按绑定顺序执行当前元素要响应的事件，然后冒泡到父节点，按照同样逻辑处理
-    - `return false` 可以阻止后续事件执行
-    - 支持的事件包括：
-        - tap
-        - click
-        - touchstart
-        - touchmove
-        - touchend
- */
-
-/** 初始化委托 */
-function initDelegate(vnode, bindEvent) {
-    if (typeof bindEvent !== 'function') {
-        return false;
+/** 事件委托 */
+class Delegate {
+    constructor(vnode) {
+        this.vnode = vnode;
+        this._map = {};
+        // 用来给委托任务编号，事件触发时按顺序执行
+        this._index = 0;
     }
-    let events = {
-        tap: { event: 'onTap', list: [] },
-        click: { event: 'onClick', list: [] },
-        touchstart: { event: 'onTouchStart', list: [] },
-        touchmove: { event: 'onTouchMove', list: [] },
-        touchend: { event: 'onTouchEnd', list: [] }
-    };
-    bindEvent({
-        on: function(eventName, selector, action) {
-            if (typeof eventName === 'string') {
-                if (!action) {
-                    action = selector;
-                    selector = null;
-                }
-                if (typeof action === 'function') {
-                    if (typeof selector !== 'string') {
-                        selector = '';
-                    }
-                    eventName
-                        .trim()
-                        .split(/\s+/)
-                        .forEach(item => {
-                            if (events[item]) {
-                                events[item].list.push({ selector, action });
+    /**
+     * 将事件处理函数添加到当前节点
+     * @param {string} events 事件名，多个事件用空格分隔
+     * @param {string} selector 选择器
+     * @param {function} handler 事件处理函数
+     */
+    on(events, selector, handler) {
+        let { _map, vnode } = this;
+        if (isFunction(selector)) {
+            handler = selector;
+            selector = null;
+        }
+        if (isString(events) && isFunction(handler)) {
+            let index = this._index++;
+            if (!isString(selector)) {
+                selector = null;
+            }
+            events.split(/\s+/).forEach(eventType => {
+                if (eventType) {
+                    if (!_map[eventType]) {
+                        _map[eventType] = [];
+                        // 事件名采用驼峰形式
+                        let eventBindName = 'on' + eventType.replace(/^\w/, w => w.toUpperCase());
+                        let oldHandler = vnode.attributes[eventBindName];
+                        if (!isFunction(oldHandler)) {
+                            oldHandler = null;
+                        }
+                        vnode.attributes[eventBindName] = event => {
+                            if (oldHandler) {
+                                oldHandler(event);
                             }
+                            this.exec(eventType, event);
+                        };
+                    }
+                    _map[eventType].push({ index, selector, handler });
+                }
+            });
+        }
+        return this;
+    }
+    /**
+     * 执行附加到节点上的指定事件的所有处理函数
+     * @param {string} eventType
+     * @param {Event} event
+     */
+    exec(eventType, event) {
+        let root = event.currentTarget;
+        let target = event.target;
+        let execList = [];
+        let list = this._map[eventType];
+        // 查找子元素命中的委托
+        while (target && root && target != root) {
+            let matches = {};
+            for (let index = 0; index < list.length; index++) {
+                let { selector, handler } = list[index];
+                if (selector) {
+                    if (matches[selector] == null) {
+                        let finds = root.querySelectorAll(selector);
+                        matches[selector] = Array.prototype.indexOf.call(finds, target) > -1;
+                    }
+                    if (matches[selector]) {
+                        execList.push({
+                            target,
+                            handler,
+                            event: Object.assign({}, event, { target })
                         });
+                    }
                 }
             }
-            return this;
+            target = target.parentElement || root;
         }
-    });
-
-    for (const key in events) {
-        const item = events[key];
-        if (item.list && item.list.length) {
-            bindDelegate(vnode.attributes, item);
-        }
-    }
-}
-
-/** 绑定委托事件 */
-function bindDelegate(attrs, { event, list }) {
-    let oldEvent = attrs[event];
-    attrs[event] = e => {
-        if (oldEvent) {
-            oldEvent(e);
-        }
-        execDelegate(e, list);
-    };
-}
-
-/** 执行委托事件 */
-function execDelegate(e, list) {
-    let root = e.currentTarget;
-    let target = e.target;
-    let execList = [];
-    // 查找子元素命中的委托
-    while (target && root && target != root) {
-        let matches = {};
-        for (let index = 0; index < list.length; index++) {
-            let { selector, action } = list[index];
-            if (selector) {
-                if (matches[selector] == null) {
-                    let finds = root.querySelectorAll(selector);
-                    matches[selector] = Array.prototype.indexOf.call(finds, target) > -1;
-                }
-                if (matches[selector]) {
-                    execList.push({
-                        elem: target,
-                        action,
-                        event: M.extend({}, e, { target })
-                    });
-                }
+        // 查找当前元素命中的委托
+        list.forEach(({ selector, handler }) => {
+            if (!selector) {
+                execList.push({ target: root, handler, event });
             }
-        }
-        target = target.parentElement || root;
-    }
-    // 查找当前元素命中的委托
-    list.forEach(({ selector, action }) => {
-        if (!selector) {
-            execList.push({ elem: root, action, event: e });
-        }
-    });
-    if (execList.length) {
+        });
+        // 依次执行命中的委托
+        execList.sort((a, b) => a.index - b.index);
         while (execList.length) {
-            let { elem, action, event } = execList.shift();
-            let ret = action.call(elem, event);
-            if (ret === false) {
-                e.preventDefault();
-                e.stopPropagation();
+            let { target, handler, event } = execList.shift();
+            // 在事件处理函数中返回 false 时，停止后续的委托处理
+            if (handler.call(target, event) === false) {
+                event.preventDefault();
+                event.stopPropagation();
                 return false;
             }
         }
     }
 }
 
+//#region 通过vnode钩子函数解析on属性，并进行事件委托
 const { options } = preact;
 let oldHook = options.vnode; // 保存之前的钩子函数
 options.vnode = vnode => {
-    // 有 data-tj 属性时进行上报
-    let attrs = vnode.attributes;
-    let bindEvent = attrs && attrs.on;
-    if (bindEvent) {
-        delete attrs.on;
-        initDelegate(vnode, bindEvent);
+    // 设置了on属性时，创建Delegate对象进行委托
+    let { attributes } = vnode;
+    if (attributes.on && isFunction(attributes.on)) {
+        attributes.on(new Delegate(vnode));
+        delete attributes.on;
     }
-
     // 执行之前的钩子函数
     if (oldHook) {
         oldHook(vnode);
     }
 };
+//#endregion
 
-export default (QMEvent => {
-    return QMEvent;
-})(window.QMEvent || {});
+export default Delegate;
